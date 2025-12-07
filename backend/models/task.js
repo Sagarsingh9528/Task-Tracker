ae3574e4435cc390095c1ab79e7c04eee5e1116d
Sagar Singh <sagarsingh33693@gmail.com>
2025-12-07 12:27:44 +0530

    Froneten done Task-Tracker

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,16 +1,32 @@
 import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true, trim: true },
-  description: { type: String, default: '' },
-  dueDate: { type: Date, default: null },
-  priority: { type: String, enum: ['low','medium','high'], default: 'low' },
-  status: { type: String, enum: ['To Do','In Progress','Done'], default: 'To Do' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-}, {timestamps: true});
+const taskSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true, trim: true },
+    description: { type: String, default: "" },
+    dueDate: { type: Date, default: null },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+
+    priority: {
+      type: String,
+      enum: ["low", "normal", "high"],
+      default: "normal",
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 
 const Task = mongoose.model("Task", taskSchema);
 
-export default Task; 
+export default Task;
